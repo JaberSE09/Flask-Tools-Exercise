d0b94da1d654d6ae14563a35dad6b8934af4ec04
Sajid Jaber <sajidjaber@yahoo.com>
2024-02-08 20:16:04 -0600

    first commit setup

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+
+# the toolbar is only enabled in debug mode:
+app.debug = True
+
+# set a 'SECRET_KEY' to enable the Flask session cookies
+app.config['SECRET_KEY'] = 'secret'
+
+toolbar = DebugToolbarExtension(app)
+
+
+@app.route('/')
+def home():
+    return "home"
+
