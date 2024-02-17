81186f596f1061e5e7d1af085067ed1f7b60bd51
Sajid Jaber <sajidjaber@yahoo.com>
2024-02-17 07:58:47 -0600

    finished

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
 from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
+
 RESPONSES_KEY = "responses"
 
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "never-tell!"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
-# the toolbar is only enabled in debug mode:
-app.debug = True
+debug = DebugToolbarExtension(app)
 
-# set a 'SECRET_KEY' to enable the Flask session cookies
-app.config['SECRET_KEY'] = 'secret'
-toolbar = DebugToolbarExtension(app)
-response = list()
 
-@app.route('/')
-def home():
+@app.route("/")
+def show_survey_start():
+    """Select a survey."""
 
-    return render_template("home.html" , survey= survey   )
+    return render_template("survey_start.html", survey=survey)
 
 
 @app.route("/begin", methods=["POST"])
@@ -26,6 +26,27 @@ def start_survey():
     session[RESPONSES_KEY] = []
 
     return redirect("/questions/0")
+
+
+@app.route("/answer", methods=["POST"])
+def handle_question():
+    """Save response and redirect to next question."""
+
+    # get the response choice
+    choice = request.form['answer']
+
+    # add this response to the session
+    responses = session[RESPONSES_KEY]
+    responses.append(choice)
+    session[RESPONSES_KEY] = responses
+
+    if (len(responses) == len(survey.questions)):
+        # They've answered all the questions! Thank them.
+        return redirect("/complete")
+
+    else:
+        return redirect(f"/questions/{len(responses)}")
+
 
 @app.route("/questions/<int:qid>")
 def show_question(qid):
@@ -55,23 +76,3 @@ def complete():
     """Survey complete. Show completion page."""
 
     return render_template("completion.html")
-
-
-@app.route("/answer", methods=["POST"])
-def handle_question():
-    """Save response and redirect to next question."""
-
-    # get the response choice
-    choice = request.form['answer']
-
-    # add this response to the session
-    responses = session[RESPONSES_KEY]
-    responses.append(choice)
-    session[RESPONSES_KEY] = responses
-
-    if (len(responses) == len(survey.questions)):
-        # They've answered all the questions! Thank them.
-        return redirect("/complete")
-
-    else:
-        return redirect(f"/questions/{len(responses)}")
